0dcf8eb77ceaa83a09177ebb909cc7fc40e1d1a2
denvermullets <devmullets@gmail.com>
2020-10-10 16:41:27 -0400

    :sparkles: setup resource and add migration

---
+++ app/models/opening.rb
@@ -0,0 +1,2 @@
+class Opening < ApplicationRecord
+end
---
+++ db/migrate/20201010193326_create_openings.rb
@@ -0,0 +1,19 @@
+class CreateOpenings < ActiveRecord::Migration[6.0]
+  def change
+    create_table :openings do |t|
+      t.string :company_name
+      t.string :job_url
+      t.string :job_title
+      t.string :salary_info
+      t.string :location
+      t.string :job_id
+      t.string :extra_info
+      t.string :company_page
+      t.string :connection
+      t.string :job_description
+      t.boolean :applied
+      t.boolean :remind_me
+      t.timestamps
+    end
+  end
+end
