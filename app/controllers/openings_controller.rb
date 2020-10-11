d6e2dde9845b01451baed670f2aeb5191b2c07b0
denvermullets <devmullets@gmail.com>
2020-10-10 22:00:18 -0400

    :construction: working on LI nodes but able to login

--- app/controllers/openings_controller.rb
+++ app/controllers/openings_controller.rb
@@ -4,7 +4,8 @@ class OpeningsController < ApplicationController
   end
 
   def linkedin
-    url = 'https://tequilamatchmaker.com/tequilas'
+    # url = 'https://tequilamatchmaker.com/tequilas'
+    url = 'https://www.linkedin.com/jobs/search/?f_TPR=r86400&geoId=90000070&keywords=full%20stack&location=New%20York%20City%20Metropolitan%20Area'
     response = Linkedin.process(url)
 
     if response[:status] == :completed && response[:error].nil?
--- app/models/linkedin.rb
+++ app/models/linkedin.rb
@@ -7,30 +7,47 @@ class Linkedin < Kimurai::Base
     self.crawl!
   end
 
-  def parse(response, url:, data: {})
+  def scrape_page
+    # Update response to current response after interaction with a browser
+    # response = browser.current_response
+
+    username = ENV['EMAIL']
+    password = ENV['PASSWORD']
+    sleep 5
+    browser.find(:css, 'a.nav__button-secondary').click
+    sleep 1
+    browser.fill_in 'session_key', with: username
+    sleep 1
+    browser.fill_in 'session_password', with: password
     sleep 3
-    doc = browser.current_response
-    returned_tequila = doc.css('div.ais-hits')
-    returned_tequila.css('div.ais-hits--item').each do |single_tequila|
-      # scrape each individual product listing url for tqdb
-      url = "https://tequilamatchmaker.com" + single_tequila.css('a').attribute('href').text 
-      # t.string :company_name
-      # t.string :job_url
-      # t.string :job_title
-      # t.string :salary_info
-      # t.string :location
-      # t.string :job_id
-      # t.string :extra_info
-      # t.string :company_page
-      # t.string :connection
-      # t.string :job_description
-      # t.boolean :applied
-      # t.boolean :remind_me
-      Opening.create(company_name: nil, job_url: url, job_title: nil, salary_info: nil, 
-        location: nil, job_id: nil, extra_info: nil, company_page: nil, connection: nil, 
-        job_description: nil, applied: false, remind_me: false)
-      puts url
-    end
+    browser.find(:css, 'button.btn__primary--large').click
+    sleep 2
     
+    # Update response to current response after interaction with a browser
+    # response = browser.current_response
+    job_listings = browser.find(:css, 'ul.jobs-search-results__list')
+    job_listings.css('li.jobs-search-results__list-item').each do |single_job|
+      job_url = job_listings.css('div div.job-card-container div.flex-grow-1 div.mr1 a')
+      byebug
+    end
+    # byebug
+    doc = 'hi'
+    # browser.save_screenshot
+  end 
+  def parse(response, url:, data: {})
+    scrape_page
+    # sleep 3
+    # doc = browser.current_response
+    # returned_tequila = doc.css('div.ais-hits')
+    # returned_tequila.css('div.ais-hits--item').each do |single_tequila|
+    #   # scrape each individual product listing url for tqdb
+    #   url = "https://tequilamatchmaker.com" + single_tequila.css('a').attribute('href').text 
+    
+    
+    # Opening.create(company_name: nil, job_url: url, job_title: nil, salary_info: nil, 
+    #   location: nil, job_id: nil, extra_info: nil, company_page: nil, connection: nil, 
+    #   job_description: nil, applied: false, remind_me: false)
+    # puts url
   end
-end 
+    
+end
